4e5eda9dc532ea2a7bacfc031eb115ece18015e2
Andy Pearson <andy@londonparkour.com>
2021-01-23 17:24:12 +0000

    updated to working

--- src/initialise.php
+++ src/initialise.php
@@ -7,6 +7,13 @@ class initialise {
 
     public function __construct()
     {
+
+        // ┌─────────────────────────────────────────────────────────────────────────┐
+        // │                            Include Field Groups    	        	     │
+        // └─────────────────────────────────────────────────────────────────────────┘
+        require __DIR__.'/acf/acf_field_groups.php';
+
+        
         new filters\disable_wysiwyg;
         new filters\the_content;
     }
