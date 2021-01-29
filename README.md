142544f0096b6d1a2c31c08918580eb91b431cf0
Andy Pearson <andy@londonparkour.com>
2021-01-29 16:47:20 +0000

    tax terms

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Page Builder
 
 Allows you to create pages, properly.
+
+## Taxonomy Pages
+
+You can load the contents of a taxonomy page like so:
+
+```php
+$term = get_queried_object();
+$pb = new andyp\pagebuilder\filters\the_content;
+$content = $pb->filter_the_content_in_the_main_loop(null, $term);
+
+echo $content;
+
+```
--- src/filters/the_content.php
+++ src/filters/the_content.php
@@ -17,17 +17,26 @@ class the_content
     }
 
     
-    public function filter_the_content_in_the_main_loop( $content ) {
+    public function filter_the_content_in_the_main_loop( $content, $term = null ) {
     
 
         // Get from ACF
-        $builder_instance = get_field('builder_instance');
+        $builder_instance = get_field('builder_instance', $term);
 
+        // Skip if a taxonomy term.
+        if (empty($term)) {
+            
+            if (!is_singular()) {
+                return $content;
+            }
+            if (!in_the_loop()) {
+                return $content;
+            }
+            if (!is_main_query()) {
+                return $content;
+            }
+        }
 
-        // Check if we're inside the main loop in a single Post.
-        if ( !is_singular() ) { return $content; }
-        if ( !in_the_loop() ) { return $content; }
-        if ( !is_main_query() ) { return $content; }
         if( empty($builder_instance) ) { return $content; }
 
         foreach ($builder_instance as $this->instance)
